335ab951e7f4382b0dc7b4899e68c03326e754b5
SamiVijarra <samivijarra1@gmail.com>
2026-01-08 02:44:27 -0300

    hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { Toaster } from 'sonner'
 
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ClientInformation } from './08-use-suspense/ClientInformation'
+import { getUserAction } from './08-use-suspense/api/get-user.action'
 //import { ScrambleWords } from './05-useReducer/ScrambleWords'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { MemoCounter } from './06-memos/MemoCounter'
-import { InstagromApp } from './07-useOptimistic/InstagramApp'
+//import { InstagromApp } from './07-useOptimistic/InstagramApp'
 //import { FocusScreen } from './03-useRef/FocusScreen'
 //import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 //import { PokemonPage } from './03-examples/PokemonPage'
@@ -28,6 +30,15 @@ createRoot(document.getElementById('root')!).render(
     {/*<FocusScreen/>*/}
     {/*<TasksApp/>*/}
     {/*<ScrambleWords/>*/}
-    <InstagromApp/>
+    {/*<InstagromApp/>*/}
+    <Suspense
+      fallback={
+      <div className="gb-gradient flex flex-col">
+        <h1 className='text-2xl'>Cargando</h1>
+        </div>
+    }
+    />
+      <ClientInformation getUser={getUserAction(1000)}/>
+    <Suspense />
   </StrictMode>,
 )
